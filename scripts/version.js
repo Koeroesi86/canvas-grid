a7ba273df77998650c433afa860530d3ef35d009
Krisztian Korosi <koeroesi86@gmail.com>
2019-07-09 22:00:46 +0100

    Cleaner semver

--- scripts/version.js
+++ scripts/version.js
@@ -2,8 +2,8 @@ const { execSync } = require('child_process');
 
 const { TRAVIS_TAG, TRAVIS_BUILD_NUMBER } = process.env;
 
-const minor = (TRAVIS_BUILD_NUMBER || '1').padStart(3, '0');
-const major = Math.floor(TRAVIS_BUILD_NUMBER / 1000);
+const major = Math.floor(TRAVIS_BUILD_NUMBER / 100);
+const minor = TRAVIS_BUILD_NUMBER  - major;
 const version = `1.${major}.${minor}-${TRAVIS_TAG || 'release'}`;
 
 execSync(`yarn version --no-git-tag-version --new-version "v${version}"`, { shell: true });
