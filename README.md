a017428b14a9b64df0f10b6b3e255c0f0ccebb97
Krisztian Korosi <koeroesi86@gmail.com>
2019-07-09 20:45:31 +0100

    Attempt to version before npm

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Canvas Grid
 
-[![Build status](https://travis-ci.com/Koeroesi86/canvas-grid.svg?branch=master)](https://travis-ci.com/Koeroesi86/canvas-grid)
+[![Build status](https://api.travis-ci.org/Koeroesi86/canvas-grid.svg?branch=master)](https://travis-ci.com/Koeroesi86/canvas-grid)
--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 const { resolve } = require('path');
 const { existsSync } = require('fs');
 const { removeSync } = require('fs-extra');
+const { execSync } = require('child_process');
 const webpack = require('webpack');
 
 process.env.BABEL_ENV = 'production';
@@ -13,6 +14,10 @@ const config = require('../webpack.config');
 if (existsSync(resolve(__dirname, '../build'))) {
   removeSync(resolve(__dirname, '../build'))
 }
+
+const { TRAVIS_TAG, TRAVIS_BUILD_NUMBER } = process.env;
+const version = `1.0.${(TRAVIS_BUILD_NUMBER || '1').padStart(3, '0')}-${TRAVIS_TAG}`;
+execSync(`export VERSION=${version}`);
 
 process.on('unhandledRejection', err => {
   throw err
